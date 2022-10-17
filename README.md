36021abf7452dc512cfacb8912222d54ed521eea
Leonardo Zaniboni <leonardozaniboni@usp.br>
2022-10-17 18:32:22 -0300

    Primeiro commit : Adicionando o separador 'main.py' junto com o I/O.

---
+++ README.md
@@ -0,0 +1,9 @@
+Autor : Leonardo Zaniboni Silva    11801049
+
+Repositório referente ao programa separador por ';' relativo a terceira atividade da disciplina de Desenvolvimento de Software para Sistemas Embarcados com Sistemas Operacionais (SEL0456). 
+
+O arquivo 'arquivo_final_com;.txt' apresenta o OUTPUT obtido. Já o arquivo 'arquivo_inicial_com,.txt' apresenta o input fornecido pelo docente. O código realizado segue descrito e comentado em 'main.py'.
+
+
+
+
---
+++ main.py
@@ -0,0 +1,37 @@
+#Leonardo Zaniboni Silva   11801049  
+
+arquivo = open("arquivo_inicial_com,.txt","r")                  #abrindo o arquivo
+dados = arquivo.read()                                          #lendo o arquivo
+arquivo.close()
+
+novos_dados = ""                                                #inicia uma string vazia
+em_string = False                                               #booleano para verificar se o caracter esta em uma string
+inverter = False                                                #boleano para alterar a variavel 'em_string' para true no final da iteração, se necessario
+
+for i in range(len(dados)):
+
+  if em_string == False :                                       #se não estiver em uma string, alterar os ',' por ';'
+      if dados[i] == ',' : 
+        novos_dados = novos_dados + dados[i].replace(",",";")
+      else:
+        novos_dados = novos_dados + dados[i]
+
+      if dados[i] == '"' and dados[i-1] != chr(92):             #se caso encontrar um '"' e o caracter anterior não for uma "\"  = os próximos caracteres fazem parte de uma string
+        inverter = True
+
+  else:
+      novos_dados = novos_dados + dados[i]                      #caso seja uma string, apenas copie os valores e não mude o ',' por ';'
+
+      if dados[i] == '"' and dados[i-1] != chr(92):             #se caso encontrar um '"' e o caracter anterior não for uma "\"  = os próximos caracteres não são mais uma string
+        inverter = True
+
+  if inverter == True and em_string == False :                  #lógica para inverter o boleano 'em_string'
+      em_string = True
+  elif inverter == True and em_string == True :
+      em_string = False
+
+  inverter = False                                              #garantindo que a cada iteração começa sem inverter
+
+arquivo = open('arquivo_final_com;.txt','a')                    #salvando os dados em um novo arquivo
+arquivo.write(novos_dados)
+arquivo.close()
